9d5c059f7306012d79f715288df518717245c3c2
Asad <52581107+Asad-ullah1@users.noreply.github.com>
2022-04-11 07:49:00 +0500

    firstcommit

---
+++ add.js
@@ -0,0 +1,53 @@
+               // Question 1
+/*
+
+let firstName = prompt("first-name"); 
+let lastName= prompt("last-name");
+let fullName= firstName + lastName;
+console.log(fullName);
+alert("Welcome "+"  "+ fullName);
+
+*/
+               // Question 2
+/*
+var text =prompt("Your fravite phone plz? ");
+var _lenth =text.length;
+
+    document.write("My fravite phone is :  " +text +"<br>");
+    document.write("length of a string : "+ _lenth);
+
+    */
+
+               // Question 3
+/*
+               let text = "Pakistani";
+              document.write("String : " + text +"<br>")
+               var result=text.indexOf("n");
+
+               document.write("IndexOf 'n' is : " +result);
+*/
+
+
+               // Question 4
+  /*
+
+  var texxt ="Hello World";
+  document.write("string :"+texxt +"<br>");
+  var _result= texxt.lastIndexOf("l");
+  document.write("Last IndexOf 'l :  "  + _result);
+
+*/  
+               // Question 5
+    var first ="pakistani";
+    document.write("String : "+ first +"<br>");
+    var _index =first.charAt("3");
+    document.write(_index);
+
+
+               // Question 6
+               // Question 7
+               // Question 8
+               // Question 9
+               // Question 10
+
+
