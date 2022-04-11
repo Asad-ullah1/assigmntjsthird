f0072276dad980badac663b0577610c4be36d3d8
Asad <52581107+Asad-ullah1@users.noreply.github.com>
2022-04-11 14:47:34 +0500

    Update add.js

--- add.js
+++ add.js
@@ -38,16 +38,56 @@ var _lenth =text.length;
 
 */  
                // Question 5
+/*
     var first ="pakistani";
     document.write("String : "+ first +"<br>");
     var _index =first.charAt("3");
     document.write(_index);
-
+*/
 
                // Question 6
+       /* 
+        var firstName = prompt("first-name"); 
+        var lastName= prompt("last-name");
+        var answer =firstName .concat(lastName);
+        document.write("using concat method :  " +answer);
+         */      
+
                // Question 7
-               // Question 8
-               // Question 9
+/*
+    var city="Hyderabad";
+    document.write("city :"+city);
+    var _replace=city.replace("Hyder","Islam"+"<br>");
+    document.write("after replace : "+_replace);
+*/
+
+              // Question 9
+
+/*
+ var value=472;
+var sting =value.toString();
+document.write(sting);
+*/
+
                // Question 10
+/*
+  var user =prompt("value plz");
+  document.write("User input : "+user+"<br>");
+  var sse =user.toUpperCase();
+  document.write("Upper case : "+sse);
+*/
 
-
+               // Question 11
+/*
+               var digt="javascript";
+               document.write("user input : "+digt+"<br>");
+               var digt2=digt.charAt(0).toUpperCase() +digt.slice(1);
+               document.write("Title case : "+digt2);
+  */
+               // Question 12
+               // Question 13
+               // Question 14
+               // Question 15
+               // Question 16
+               // Question 17
+               // Question 18
